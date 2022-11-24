7ffdc8bc65a83f99d7edf7c5325f78780dfca749
Dimitar <wopz1337@gmail.com>
2022-11-24 12:23:08 +0200

    Added Folder & File Structure And Import Components In App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, NavBar } from "./components";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>GPT-3</h1>
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <NavBar />
+      <Header />
     </div>
-  );
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
